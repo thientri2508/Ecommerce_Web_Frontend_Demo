96da78f2ae191560013b4437dca0c20b16d2daaf
thientri2508 <ynhi572@gmail.com>
2024-12-18 11:21:44 +0700

    update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Ecommerce_Web_Frontend_Demo/',
   optimizeDeps: {
       include: ['react-quill'],
   },
